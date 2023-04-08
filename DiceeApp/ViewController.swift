d3e4683c3d5431142cfccc3677aaf1c3c153b37d
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-08 20:47:57 +0300

    Dice Outlets added

--- DiceeApp/ViewController.swift
+++ DiceeApp/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImageViewOne: UIImageView!
+    @IBOutlet var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 
 
